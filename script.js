bb52f0698debfa765522bd86454907f4040fced2
enigma9119 <enigma9119@users.noreply.github.com>
2023-08-26 20:33:01 -0700

    Play a single round of the game

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 "use strict";
 
-let choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  return Math.floor(Math.random() * choices.length);
+  const index = Math.floor(Math.random() * choices.length);
+  return choices[index];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  let result;
+
+  if (playerSelection === "rock") {
+    switch (computerSelection) {
+      case "rock":
+        result = "Its a Tie!";
+        break;
+      case "paper":
+        result = "You Lose! Paper beats Rock";
+        break;
+      case "scissors":
+        result = "You Win! Rock beats Scissors";
+        break;
+    }
+  } else if (playerSelection === "paper") {
+    switch (computerSelection) {
+      case "rock":
+        result = "You Win! Paper beats Rock";
+        break;
+      case "paper":
+        result = "Its a Tie!";
+        break;
+      case "scissors":
+        result = "You Lose! Scissors beats Paper";
+        break;
+    }
+  } else if (playerSelection === "scissors") {
+    switch (computerSelection) {
+      case "rock":
+        result = "You Lose! Rock beats Scissors";
+        break;
+      case "paper":
+        result = "You Win! Paper beats Scissors";
+        break;
+      case "scissors":
+        result = "Its a Tie!";
+        break;
+    }
+  }
+
+  return result;
+}
+
+const playerSelection = prompt("What do you choose?").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
