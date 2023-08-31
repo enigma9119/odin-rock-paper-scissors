ee9b8728a12bf05386a4c5d0cb983807e47f9236
enigma9119 <enigma9119@users.noreply.github.com>
2023-08-31 02:20:08 -0700

    Add an interface for the game
    Keep score and end the game when either player reaches a max score of 5.
    Display the results of each round and the final outcome.

--- script.js
+++ script.js
@@ -1,76 +1,73 @@
 "use strict";
 
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
   const index = Math.floor(Math.random() * choices.length);
   return choices[index];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(event) {
+  let playerSelection = this.textContent;
+  const computerSelection = getComputerChoice();
+  const result = document.querySelector(".result");
+
   // Exit early if the selections are equal
   if (playerSelection === computerSelection) {
-    console.log("Its a Tie!");
-    return "tie";
+    result.textContent = "Its a tie!";
+    return;
   }
 
   // Determine the winner of the round
   let winner;
-  if (playerSelection === "rock") {
-    winner = computerSelection === "scissors" ? "player" : "computer";
-  } else if (playerSelection === "paper") {
-    winner = computerSelection === "rock" ? "player" : "computer";
-  } else if (playerSelection === "scissors") {
-    winner = computerSelection === "paper" ? "player" : "computer";
+  if (playerSelection === "Rock") {
+    winner = computerSelection === "Scissors" ? "player" : "computer";
+  } else if (playerSelection === "Paper") {
+    winner = computerSelection === "Rock" ? "player" : "computer";
+  } else if (playerSelection === "Scissors") {
+    winner = computerSelection === "Paper" ? "player" : "computer";
   }
 
   // Display the round results
   if (winner === "player") {
-    console.log(`${playerSelection} beats ${computerSelection}!`);
+    result.textContent = `${playerSelection} beats ${computerSelection}!`;
   } else {
-    console.log(`${computerSelection} beats ${playerSelection}!`);
+    result.textContent = `${computerSelection} beats ${playerSelection}!`;
   }
 
-  return winner;
-}
+  updateScores(winner);
 
-function getGameOutcome(playerScore, computerScore) {
-  if (playerScore > computerScore) {
-    return "You Win!!";
-  } else if (playerScore < computerScore) {
-    return "You Lose!";
-  } else {
-    return "Its a Tie!";
+  if (playerScore === MAX_SCORE || computerScore === MAX_SCORE) {
+    gameOver();
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; ++i) {
-    let playerSelection = prompt("What do you choose?");
-    if (!playerSelection || !choices.includes(playerSelection)) {
-      playerSelection = "rock";
-    } else {
-      playerSelection.toLowerCase();
-    }
-
-    const computerSelection = getComputerChoice();
-
-    const winner = playRound(playerSelection, computerSelection);
-    if (winner === "player") {
-      ++playerScore;
-    } else if (winner === "computer") {
-      ++computerScore;
-    }
-
-    console.log(
-      `Current Score => You: ${playerScore}, Computer: ${computerScore}`
-    );
-  }
-
-  console.log(getGameOutcome(playerScore, computerScore));
+function gameOver() {
+  const result = document.querySelector(".result");
+  result.textContent = playerScore === MAX_SCORE ? "YOU WIN!!" : "YOU LOSE!!";
+  stopGame();
 }
 
-game();
+function updateScores(winner) {
+  if (winner === "player") {
+    ++playerScore;
+  } else if (winner === "computer") {
+    ++computerScore;
+  }
+
+  const playerScoreElement = document.querySelector(".player-score");
+  const computerScoreElement = document.querySelector(".computer-score");
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+}
+
+function stopGame() {
+  buttons.forEach((button) => button.removeEventListener("click", playRound));
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const MAX_SCORE = 5;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", playRound));
